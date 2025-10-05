8d3c3f4106cbb82220053707153eacd2b77c8151
Stalwart <Abdulsalamakintaro@gmail.com>
2025-10-05 12:06:44 +0100

    First statging

---
+++ script/Signup.js
@@ -0,0 +1,16 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAEWuDKVz2FGRRggvHE_FoAcTBCBxMwiyE",
+    authDomain: "medical-appointment-98ddc.firebaseapp.com",
+    projectId: "medical-appointment-98ddc",
+    storageBucket: "medical-appointment-98ddc.firebasestorage.app",
+    messagingSenderId: "461623670875",
+    appId: "1:461623670875:web:e517ca0b69eaee8a4ea77e"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
